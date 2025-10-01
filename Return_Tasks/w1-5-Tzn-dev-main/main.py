12dd7dc7b75b4255e71c6f2d231450a98723dce1
Tzn-dev <Tomi.Nikkinen.1@student.lab.fi>
2025-10-01 18:27:38 +0300

    Week_3

--- Return_Tasks/w1-5-Tzn-dev-main/main.py
+++
@@ -1,8 +0,0 @@
-print("Calculate the area of a wall.")
-Feed = input("Enter the width in meters: ")
-Width = int(Feed)
-Feed2 = input("Enter the heightin meters: ")
-Height = int(Feed2)
-print("Width is", Width,"m and height is", Height,"m.")
-Area = Height * Width
-print("The wall will be", Area, "square meters.")
